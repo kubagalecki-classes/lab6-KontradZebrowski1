c8974d150183419ba724886de7aadaa215a6fede
KontradZebrowsk <konzebr@gmail.com>
2020-12-11 22:48:18 +0000

    referencje

--- zal/foo.hpp
+++ zal/foo.hpp
@@ -10,9 +10,9 @@ std::vector< char > foo(std::list< Human >& people)
 {
     std::vector< char > ret_v(people.size());
 
-    std::for_each(people.begin(), people.end(), [&](Human h){h.birthday();});
+    std::for_each(people.begin(), people.end(), [&](Human& h){h.birthday();});
 
-    std::transform(people.begin(), people.end(), ret_v.rbegin(), [&](Human h){return h.isMonster() ? 'n' : 'y';});
+    std::transform(people.begin(), people.end(), ret_v.rbegin(), [&](Human& h){return h.isMonster() ? 'n' : 'y';});
 
 
         // std::for_each(Human p : people) { p.birthday; }
