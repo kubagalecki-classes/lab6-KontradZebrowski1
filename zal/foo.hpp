670c88fb8fe178ba5ae5b289aba037e23d27326c
KontradZebrowsk <konzebr@gmail.com>
2020-12-11 22:43:52 +0000

    possible solution?

--- zal/foo.hpp
+++ zal/foo.hpp
@@ -9,7 +9,12 @@
 std::vector< char > foo(std::list< Human >& people)
 {
     std::vector< char > ret_v(people.size());
-    std::for_each(people.rbegin(), people.rend(), birthday)
+    
+    std::for_each(people.begin(), people.end(), [&](Human h){h.birthday();});
+
+    std::transform(people.begin(), people.end(), ret_v.rbegin(), [&](Human h){return h.isMonster() ? 'n' : 'y'});
+
+
         // std::for_each(Human p : people) { p.birthday; }
         // Twoja implementacja tutaj
 
